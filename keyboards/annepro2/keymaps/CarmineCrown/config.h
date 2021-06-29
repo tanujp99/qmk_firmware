c51073aeefcc50b16066b3500a549e396e740fd0
Tanuj Palaspagar <tanuj.palaspagar@gmail.com>
2021-06-30 02:11:18 +0530

    Layer_2 edits
    optimised layout on layer 2
    base layer changed to esc, for using tap-arrows on right bottom cluster
    added tab for ease of access

--- keyboards/annepro2/keymaps/CarmineCrown/config.h
+++ keyboards/annepro2/keymaps/CarmineCrown/config.h
@@ -1,5 +1,13 @@
-// Obins stock firmware has something similar to this already enabled, but disabled by default in QMK
+#pragma once
+
 #define PERMISSIVE_HOLD
+
+#undef TAPPING_TERM
 #define TAPPING_TERM 140
+
+// #define RETRO_TAPPING
+
+#undef TAPPING_TOGGLE
+#define TAPPING_TOGGLE 3
 
 #define CAPS_LOCATION (MATRIX_COLS * 0 + 0)
